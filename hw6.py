c974d5cff6fb01924173b337c15b48e944fbcf1a
labyrinth0409 <163734592+labyrinth0409@users.noreply.github.com>
2024-04-19 17:51:06 +0900

    Add files via upload

---
+++ hw6.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+for i in range(1, 10):  
+    for j in range(2, 6): 
+        print(f'{j} x {i} = {i*j}', end='\t')
+    print()  
+
+print()  
+
+for i in range(1, 10):  
+    for j in range(6, 10):  
+        print(f'{j} x {i} = {i*j}', end='\t')
+    print()  
+
+
+# In[ ]:
+
+
+
+
